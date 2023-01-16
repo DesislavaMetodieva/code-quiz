1e752e9de04b1a4a9c7ae08a0d5c71aa02ac46e1
DesislavaMetodieva <82023025+DesislavaMetodieva@users.noreply.github.com>
2023-01-16 12:32:46 +0000

    Added setTime function

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,5 +1,3 @@
-// Psuedo code
-
 // Declare variable array for the questions
 
 // 5 questions with 4 possible answers - 1 correct and 3 wrongs
@@ -30,71 +28,132 @@ var questions = [
     },
   ];
 
-  
-  // Event listeners variables
 
   var containerQuestionEl = document.getElementById("questions");
-  var containerStartEl = document.getElementById("wrapper");
+  var containerStartEl = document.getElementById("start-screen");
   var containerEndEl = document.getElementById("end-screen");
   // var containerScoreEl = document.getElementById("score-banner")
-//   var formInitials = document.getElementById("initials")
-//   var containerHighScoresEl = document.getElementById("high-score-container")
-//   var ViewHighScoreEl = document.getElementById("view-high-scores")
+  var formInitials = document.getElementById("initials")
+  var containerHighScoresEl = document.getElementById("highscores")
+  var ViewHighScoreEl = document.getElementById("scores")
 //   var listHighScoreEl = document.getElementById("high-score-list")
 
-// Will configure later 
+// Will configure later the text below the answers to show correct or wrong depending on the user choosen answer
 
 //   var correctEl = document.getElementById("correct")
 //   var wrongEl = document.getElementById("wrong")
 
  // Buttons
 
- var btnStartEl = document.getElementById("#start");
- var btnGoBackEl = document.getElementById("#go-back")
- var btnClearScoresEl = document.getElementById("#clear")
+var btnStartEl = document.getElementById("#start");
+var btnGoBackEl = document.getElementById("#go-back")
+var btnClearScoresEl = document.getElementById("#clear")
 
  // Timer, question and answer variables
 
- var questionElement = document.getElementById("questions")
- var answerbuttonsElement = document.getElementById("choices")
- var timerElement = document.getElementById("#time");
+var questionElement = document.getElementById("questions");
+var answerbuttonsElement = document.getElementById("choices");
+var timerElement = document.getElementById("#time");
 
-  // Time variable
+// High Score
 
-  var timeLeft;
+var highScore = [];
 
-  // Other variables
+// Other variables
 
-  var gameOver;
+var gameOver;
 
-  // High Score
+// timerElement.innerText = 0;
 
-  var highScore = [];
+var timeLeft;
+
+var setTime = function () {
+    timeleft = 75;
+
+    var timercheck = setInterval(function() {
+        timerElement.innerText = timeleft;
+        timeleft--
+    
+        if (gameOver) {
+            clearInterval(timercheck);
+        }
+       
+        if (timeleft < 0) {
+            showScore()
+            timerElement.innerText = 0
+            clearInterval(timercheck)
+        }
+    
+        }, 1000)
+    };
+
+
+
 
   
 
-// Declare showQuestion function
+// Declare startGame function
 
-// Create button event listener to call / execute the function showQuestion
+var startGames = function() {
+
+//add classes to show/hide start and quiz screen
+
+    containerStartEl.classList.add('hide');
+    containerStartEl.classList.remove('show');
+    containerQuestionEl.classList.remove('hide');
+    containerQuestionEl.classList.add('show');
+    setTime()
+    setQuestion()
+
+};
+
+  //set next question for quiz
+
+  var setQuestion = function() {
+    resetAnswers();
+    displayQuestion();
+};
+
+ //remove answer buttons
+    var resetAnswers = function() {
+        while (answerbuttonsElement.firstChild) {
+            answerbuttonsElement.removeChild(answerbuttonsElement.firstChild)
+        };
+    };
 
 // Link the audio effects to the correct and wrong answers when chosen
 
 // When pressed Start quiz, the function to start presenting the questions should be called
 
-
-// Same time the timer should start from 75 seconds
-
-var setTime = function () {
-    timeleft = 75;
-};
-
-
-// Every time when the player selects a correct answer, the function continues with the next question, without any action. The timer goes on, However if the player selects a wrong answer, the time should deduct 10 seconds from the left time.
 // We need to link the Correct audio and Incorrect audio to the corresponding answers - dinamycally
 
+
+
 // No validation is needed on the submit form for the name
+
 // Once the function for showQuestion is finished OR the timer ends, the ALL DONE page comes where the end user needs to add their names (no validation required)
 
 // The user initials and Score should be stored in Local storage
+
+// showScore();
+
 // That data should be stored in the Highscores section too
+
+// createHighScore ();
+
+// on start click, start game
+// btnStartEl.addEventListener("click", startGames);
+// console.log(mewo);
+
+// //on submit button -- enter or click
+// formInitials.addEventListener("submit", createHighScore);
+
+// //when view high-scores is clicked
+// ViewHighScoreEl.addEventListener("click", displayHighScores);
+
+// //Go back button
+// btnGoBackEl.addEventListener("click", renderStartPage);
+
+// //clear scores button
+// btnClearScoresEl.addEventListener("click", clearScores);
 
